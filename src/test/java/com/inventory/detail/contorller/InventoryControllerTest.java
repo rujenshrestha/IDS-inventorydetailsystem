5dc3d9a3874aa480a16c62f2943d2d4c287eabb6
rujenshrestha <rujen.shrestha89@gmail.com>
2026-03-11 11:57:27 +0545

    Removed import

--- src/test/java/com/inventory/detail/contorller/InventoryControllerTest.java
+++ src/test/java/com/inventory/detail/contorller/InventoryControllerTest.java
@@ -7,7 +7,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
